969f7668dc6400982874c1350f4c459309de6ac7
pravin-sarule <pravin.sarule@nexintelai.com>
2025-10-14 17:27:48 +0530

    .

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:4000/api';
+const API_BASE_URL = 'https://super-adimn-backend-110685455967.asia-south1.run.app/api';
 
 const ADMIN_CREATE_URL = `${API_BASE_URL}/admins/create`;
 const ADMIN_GET_ALL_URL = `${API_BASE_URL}/admins`;
